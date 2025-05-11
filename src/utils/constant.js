7ab1eb3092d35f02ebb545fc32033b186b59f14f
son <sonnguyentk269@gmail.com>
2025-05-11 16:53:23 +0700

    fix BUILD_MODE

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,13 @@
-// export const API_ROOT = 'http://localhost:8017'
-export const API_ROOT = 'https://trello-clone-api-qq38.onrender.com'
+let apiRoot = ''
+
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-clone-api-qq38.onrender.com'
+}
+export const API_ROOT = apiRoot
 
 export const DEFAULT_PAGE = 1
 export const DEFAULT_ITEMS_PER_PAGE = 12
