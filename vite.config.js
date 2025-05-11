08bee9316a59ba0af686717f7aa5ac829d35d3e6
son <sonnguyentk269@gmail.com>
2025-05-11 17:52:48 +0700

    fix #1

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   define: {
     'process.env': process.env
@@ -11,5 +10,4 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '~', replacement: '/src' }]
   }
-  // base: './'
 })
